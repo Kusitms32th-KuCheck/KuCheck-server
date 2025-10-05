7393f8f76339688b1a1b5fd7403ea6513d5e0ac6
김민지 <minzikx@sookmyung.ac.kr>
2025-10-05 20:27:50 +0900

    chore: 불필요한 import 제거 #7

--- src/main/kotlin/onku/backend/domain/admin/service/AdminService.kt
+++ src/main/kotlin/onku/backend/domain/admin/service/AdminService.kt
@@ -3,7 +3,6 @@ package onku.backend.domain.admin.service
 import onku.backend.domain.admin.dto.MemberApprovalResponse
 import onku.backend.domain.member.enums.ApprovalStatus
 
-
 interface AdminService {
     fun updateApproval(memberId: Long, targetStatus: ApprovalStatus): MemberApprovalResponse
 }
