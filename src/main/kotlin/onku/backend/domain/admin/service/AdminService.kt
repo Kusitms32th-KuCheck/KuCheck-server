b854c80440ade8be806a115f1a14b04a8a795724
김민지 <minzikx@sookmyung.ac.kr>
2025-10-05 18:29:46 +0900

    feat: approval status 필드값 수정 로직 추가 #7

---
+++ src/main/kotlin/onku/backend/domain/admin/service/AdminService.kt
@@ -0,0 +1,9 @@
+package onku.backend.domain.admin.service
+
+import onku.backend.domain.admin.dto.MemberApprovalResponse
+import onku.backend.domain.member.enums.ApprovalStatus
+
+
+interface AdminService {
+    fun updateApproval(memberId: Long, targetStatus: ApprovalStatus): MemberApprovalResponse
+}
