3705e4af0d83385220cad71b32e3dcbcb37053ca
김민지 <minzikx@sookmyung.ac.kr>
2025-10-05 18:23:47 +0900

    feat: member 공통 에러처리용 enum 추가 #7

---
+++ src/main/kotlin/onku/backend/domain/member/MemberErrorCode.kt
@@ -0,0 +1,13 @@
+package onku.backend.domain.member
+
+import onku.backend.global.exception.ApiErrorCode
+import org.springframework.http.HttpStatus
+
+enum class MemberErrorCode(
+    override val errorCode: String,
+    override val message: String,
+    override val status: HttpStatus
+) : ApiErrorCode {
+    MEMBER_NOT_FOUND("MEMBER404", "회원 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    INVALID_MEMBER_STATE("MEMBER409", "현재 상태에서는 요청한 상태 변경을 수행할 수 없습니다.", HttpStatus.CONFLICT),
+}
