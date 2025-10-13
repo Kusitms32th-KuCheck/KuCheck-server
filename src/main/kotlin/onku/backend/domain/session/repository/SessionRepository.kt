1c51b6e39426a77fbb28d335287094674526fe6e
김민지 <minzikx@sookmyung.ac.kr>
2025-10-13 16:10:22 +0900

    feat: 현재 열려있는 세션을 검사하는 로직 추가 #13

---
+++ src/main/kotlin/onku/backend/domain/session/repository/SessionRepository.kt
@@ -0,0 +1,23 @@
+package onku.backend.domain.session.repository
+
+import onku.backend.domain.session.Session
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import java.time.LocalDateTime
+
+interface SessionRepository : CrudRepository<Session, Long> {
+
+    @Query(
+        value = """
+        SELECT *
+        FROM session s
+        WHERE :now BETWEEN TIMESTAMPADD(SECOND, -s.open_grace_seconds, s.start_time)
+                        AND TIMESTAMPADD(SECOND,  s.close_grace_seconds, s.end_time)
+        ORDER BY s.start_time DESC
+        LIMIT 1
+        """,
+        nativeQuery = true
+    )
+    fun findOpenSession(@Param("now") now: LocalDateTime): Session?
+}
