7f17ea4853594c17db4f9f000803ebdf74428946
김민지 <minzikx@sookmyung.ac.kr>
2025-10-14 14:34:18 +0900

    feat: Role의 종류와 각 Role이 가지는 권한을 하나의 enum에서 관리하도록 수정 #13

--- src/main/kotlin/onku/backend/domain/member/enums/Role.kt
+++ src/main/kotlin/onku/backend/domain/member/enums/Role.kt
@@ -1,3 +1,12 @@
 package onku.backend.domain.member.enums
 
-enum class Role { USER, ADMIN }
+enum class Role {
+    GUEST, USER, ADMIN, MANAGEMENT;
+
+    fun authorities(): List<String> = when (this) {
+        GUEST -> listOf("GUEST")
+        USER -> listOf("USER")
+        ADMIN -> listOf("ADMIN","USER")
+        MANAGEMENT -> listOf("MANAGEMENT","ADMIN","USER")
+    }
+}
