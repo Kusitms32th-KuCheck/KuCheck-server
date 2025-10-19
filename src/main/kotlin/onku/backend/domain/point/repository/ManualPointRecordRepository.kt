a79ace0c04c70a711beba8c57a8b472f8080d7e4
김민지 <minzikx@sookmyung.ac.kr>
2025-10-19 20:10:31 +0900

    feat: 사용자들의 스터디, 큐포터즈 현황 조회 #32

---
+++ src/main/kotlin/onku/backend/domain/point/repository/ManualPointRecordRepository.kt
@@ -0,0 +1,8 @@
+package onku.backend.domain.point.repository
+
+import onku.backend.domain.point.ManualPoint
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ManualPointRecordRepository : JpaRepository<ManualPoint, Long> {
+    fun findByMemberIdIn(memberIds: Collection<Long>): List<ManualPoint>
+}
