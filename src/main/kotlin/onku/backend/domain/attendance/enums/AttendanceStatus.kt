36827abe85cf72e50135f5275c7ce1fdef694643
김민지 <minzikx@sookmyung.ac.kr>
2025-10-19 20:17:52 +0900

    feat: 출석 상태 enum에 각 상태 별 출석점수 point 추가 #32

--- src/main/kotlin/onku/backend/domain/attendance/enums/AttendanceStatus.kt
+++ src/main/kotlin/onku/backend/domain/attendance/enums/AttendanceStatus.kt
@@ -3,23 +3,25 @@ package onku.backend.domain.attendance.enums
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "출석 상태")
-enum class AttendanceStatus {
-
+enum class AttendanceStatus(
+    @Schema(description = "해당 출석 상태의 점수")
+    val points: Int
+) {
     @Schema(description = "출석(공휴일 세션): +1점")
-    PRESENT_HOLIDAY,
+    PRESENT_HOLIDAY(1),
 
     @Schema(description = "출석(정시): 0점")
-    PRESENT,
+    PRESENT(0),
 
     @Schema(description = "사유서 인정(공결): 0점")
-    EXCUSED,
+    EXCUSED(0),
 
     @Schema(description = "결석(미제출): -3점")
-    ABSENT,
+    ABSENT(-3),
 
     @Schema(description = "결석(사유서 제출): -2점")
-    ABSENT_WITH_DOC,
+    ABSENT_WITH_DOC(-2),
 
     @Schema(description = "지각: -1점")
-    LATE
+    LATE(-1);
 }
