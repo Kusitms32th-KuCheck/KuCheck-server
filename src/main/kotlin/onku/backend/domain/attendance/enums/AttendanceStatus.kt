69cca7388d36e5204461e6bb11b02ef0527783bb
김민지 <minzikx@sookmyung.ac.kr>
2025-10-19 19:56:48 +0900

    feat: 출석 상태 종류 enum 값 추가 및 문서화 수정 #32

--- src/main/kotlin/onku/backend/domain/attendance/enums/AttendanceStatus.kt
+++ src/main/kotlin/onku/backend/domain/attendance/enums/AttendanceStatus.kt
@@ -2,12 +2,24 @@ package onku.backend.domain.attendance.enums
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(
-    description = "출석 상태",
-    example = "PRESENT"
-)
+@Schema(description = "출석 상태")
 enum class AttendanceStatus {
-    @Schema(description = "출석(정시)") PRESENT,
-    @Schema(description = "결석") ABSENT,
-    @Schema(description = "지각") LATE
+
+    @Schema(description = "출석(공휴일 세션): +1점")
+    PRESENT_HOLIDAY,
+
+    @Schema(description = "출석(정시): 0점")
+    PRESENT,
+
+    @Schema(description = "사유서 인정(공결): 0점")
+    EXCUSED,
+
+    @Schema(description = "결석(미제출): -3점")
+    ABSENT,
+
+    @Schema(description = "결석(사유서 제출): -2점")
+    ABSENT_WITH_DOC,
+
+    @Schema(description = "지각: -1점")
+    LATE
 }
