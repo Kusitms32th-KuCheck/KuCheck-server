f6ecb4e8948046921abf5fc1a7535e52d2cec1ca
김민지 <minzikx@sookmyung.ac.kr>
2025-10-24 19:58:04 +0900

    feat: dto 수정 및 추가 #48

--- src/main/kotlin/onku/backend/domain/member/dto/OnboardingRequest.kt
+++ src/main/kotlin/onku/backend/domain/member/dto/OnboardingRequest.kt
@@ -1,5 +1,6 @@
 package onku.backend.domain.member.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import onku.backend.domain.member.enums.Part
 
@@ -8,5 +9,11 @@ data class OnboardingRequest (
     @field:NotBlank val school: String,
     @field:NotBlank val major: String,
     val part: Part,
-    val phoneNumber: String? = null
+    val phoneNumber: String? = null,
+
+    @Schema(description = "FCM 토큰", example = "eYJhbGciOi...")
+    val fcmToken: String? = null,
+
+    @Schema(description = "프로필 이미지 URL", example = "https://s3.../member_profile/1/uuid/profile.png")
+    val profileImage: String? = null
 )
