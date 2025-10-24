413a1e1fd311e348dc3132ebe0834a713f97e398
김민지 <minzikx@sookmyung.ac.kr>
2025-10-24 20:34:10 +0900

    feat: 응답값 필드 추가에 따라 dto 수정 #49

--- src/main/kotlin/onku/backend/domain/attendance/dto/AttendanceTokenResponse.kt
+++ src/main/kotlin/onku/backend/domain/attendance/dto/AttendanceTokenResponse.kt
@@ -1,8 +1,13 @@
 package onku.backend.domain.attendance.dto
 
+import onku.backend.domain.member.enums.Part
 import java.time.LocalDateTime
 
-data class AttendanceTokenResponse (
+data class AttendanceTokenResponse(
     val token: String,
-    val expAt: LocalDateTime
+    val expAt: LocalDateTime,
+    val name: String,
+    val part: Part,
+    val school: String?,
+    val profileImageUrl: String?
 )
