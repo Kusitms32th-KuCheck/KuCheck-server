db5f1140fa0670c887f0f01965d2dd370af4f735
김영록 <praoo800@konkuk.ac.kr>
2025-10-27 13:31:22 +0900

    refactor : 시간 유틸 함수에서 공통상수 전역으로 전환 #52

--- src/main/kotlin/onku/backend/global/time/TimeRangeUtil.kt
+++ src/main/kotlin/onku/backend/global/time/TimeRangeUtil.kt
@@ -1,26 +1,36 @@
 package onku.backend.global.time
 
-import java.time.LocalDateTime
-import java.time.YearMonth
-import java.time.ZoneId
+import java.time.*
+import java.time.temporal.TemporalAdjusters
 
 object TimeRangeUtil {
+    private val ZONE: ZoneId = ZoneId.of("Asia/Seoul")
     data class MonthRange(
         val startOfMonth: LocalDateTime,
         val startOfNextMonth: LocalDateTime
     )
+    data class WeekRange(
+        val startOfWeek: LocalDate,
+        val endOfWeek: LocalDate
+    )
 
-    fun getCurrentMonthRange(zoneId: ZoneId = ZoneId.of("Asia/Seoul")): MonthRange {
+    fun getCurrentMonthRange(zoneId: ZoneId = ZONE): MonthRange {
         val now = LocalDateTime.now(zoneId)
         val startOfMonth = now.toLocalDate().withDayOfMonth(1).atStartOfDay()
         val startOfNextMonth = startOfMonth.plusMonths(1)
         return MonthRange(startOfMonth, startOfNextMonth)
     }
 
-    fun monthRange(year: Int, month: Int, zoneId: ZoneId = ZoneId.of("Asia/Seoul")): MonthRange {
+    fun monthRange(year: Int, month: Int, zoneId: ZoneId = ZONE): MonthRange {
         val ym = YearMonth.of(year, month)
         val startZ = ym.atDay(1).atStartOfDay(zoneId)
         val nextStartZ = startZ.plusMonths(1)
         return MonthRange(startZ.toLocalDateTime(), nextStartZ.toLocalDateTime())
+    }
+
+    fun thisWeekRange(today: LocalDate = LocalDate.now(ZONE)) : WeekRange {
+        val startOfWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY))
+        val endOfWeek = today.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY))
+        return WeekRange(startOfWeek, endOfWeek)
     }
 }
