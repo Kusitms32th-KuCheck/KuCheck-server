de76034f8a57d1690d5360e93ba9fb55431924ff
김민지 <minzikx@sookmyung.ac.kr>
2025-11-05 23:36:01 +0900

    chore: AbsenceApprovedType 상수 별 설명 추가 #81

--- src/main/kotlin/onku/backend/domain/absence/enums/AbsenceApprovedType.kt
+++ src/main/kotlin/onku/backend/domain/absence/enums/AbsenceApprovedType.kt
@@ -1,5 +1,23 @@
 package onku.backend.domain.absence.enums
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 enum class AbsenceApprovedType {
-    EXCUSED, ABSENT, LATE, ABSENT_WITH_DOC, ABSENT_WITH_CAUSE, EARLY_LEAVE
+    @Schema(description = "사유서 인정(공결): 0점")
+    EXCUSED,
+
+    @Schema(description = "결석(미제출): -3점")
+    ABSENT,
+
+    @Schema(description = "결석(사유서 제출): -2점")
+    ABSENT_WITH_DOC,
+
+    @Schema(description = "기타 사유(사유서 제출): -1점")
+    ABSENT_WITH_CAUSE,
+
+    @Schema(description = "지각: -1점")
+    LATE,
+
+    @Schema(description = "조퇴: -1점")
+    EARLY_LEAVE
 }
