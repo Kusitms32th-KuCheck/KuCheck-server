c4aab9711b3e5a7fe968195a7890510aca7c265a
김민지 <minzikx@sookmyung.ac.kr>
2025-11-11 11:04:19 +0900

    refactor: request 값에 YearMonth 값 추가 #89

--- src/main/kotlin/onku/backend/domain/point/dto/UpdateManualPointRequest.kt
+++ src/main/kotlin/onku/backend/domain/point/dto/UpdateManualPointRequest.kt
@@ -2,11 +2,17 @@ package onku.backend.domain.point.dto
 
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
 
 sealed interface UpdateManualPointRequest
 
 data class ToggleMemberRequest(
-    @field:NotNull val memberId: Long? = null
+    @field:NotNull
+    val memberId: Long,
+
+    @field:NotBlank
+    @field:Pattern(regexp = """^\d{4}-(0[1-9]|1[0-2])$""", message = "yearMonth는 yyyy-MM 형식이어야 합니다.")
+    val yearMonth: String
 )
 
 data class UpdateKupportersPointsRequest(
