145fb1f3a516ac6f807933bbe6885475b89ef32f
김민지 <minzikx@sookmyung.ac.kr>
2025-11-14 17:47:27 +0900

    fix: 공지목록 조회 방식 수정 #117

--- src/main/kotlin/onku/backend/domain/notice/repository/NoticeRepository.kt
+++ src/main/kotlin/onku/backend/domain/notice/repository/NoticeRepository.kt
@@ -1,6 +1,11 @@
 package onku.backend.domain.notice.repository
 
 import onku.backend.domain.notice.Notice
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface NoticeRepository : JpaRepository<Notice, Long>
+interface NoticeRepository : JpaRepository<Notice, Long> {
+
+    @EntityGraph(attributePaths = ["categories", "files"])
+    fun findAllByOrderByPublishedAtDescIdDesc(): List<Notice>
+}
