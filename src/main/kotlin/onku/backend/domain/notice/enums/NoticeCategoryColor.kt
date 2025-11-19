1d63c0b9313893f74c6a26101085fed4b5e24e02
김민지 <minzikx@sookmyung.ac.kr>
2025-11-19 14:31:39 +0900

    refactor: 기획 요구에 맞춰서 공지 카테고리 색상 종류 수정 #139

--- src/main/kotlin/onku/backend/domain/notice/enums/NoticeCategoryColor.kt
+++ src/main/kotlin/onku/backend/domain/notice/enums/NoticeCategoryColor.kt
@@ -1,5 +1,36 @@
 package onku.backend.domain.notice.enums
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "공지 카테고리 색상")
 enum class NoticeCategoryColor {
-    RED, ORANGE, YELLOW, GREEN, TEAL, BLUE, INDIGO, PURPLE, PINK, BROWN, GRAY
+    @Schema(description = "빨강")
+    RED,
+
+    @Schema(description = "주황")
+    ORANGE,
+
+    @Schema(description = "노랑")
+    YELLOW,
+
+    @Schema(description = "초록")
+    GREEN,
+
+    @Schema(description = "연두")
+    LIGHT_GREEN,
+
+    @Schema(description = "청록")
+    TEAL,
+
+    @Schema(description = "파랑")
+    BLUE,
+
+    @Schema(description = "보라")
+    PURPLE,
+
+    @Schema(description = "분홍")
+    PINK,
+
+    @Schema(description = "갈색")
+    BROWN;
 }
