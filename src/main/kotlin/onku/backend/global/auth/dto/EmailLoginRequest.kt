f2350bb36833c52beed24492c41505eb0461e945
김민지 <minzikx@sookmyung.ac.kr>
2026-01-17 17:07:27 +0900

    feat: 로그인, 회원가입 요청용 dto 추가 #182

--- src/main/kotlin/onku/backend/global/auth/dto/EmailLoginRequest.kt
+++ src/main/kotlin/onku/backend/global/auth/dto/EmailLoginRequest.kt
@@ -1,4 +1,16 @@
 package onku.backend.global.auth.dto
 
-class EmailLoginRequest {
-}
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
+data class EmailLoginRequest(
+    @field:Email(message = "올바른 이메일 형식이어야 합니다.")
+    @field:NotBlank(message = "이메일은 필수입니다.")
+    @Schema(example = "test@gmail.com")
+    val email: String,
+
+    @field:NotBlank(message = "비밀번호는 필수입니다.")
+    @Schema(example = "test1234")
+    val password: String
+)
